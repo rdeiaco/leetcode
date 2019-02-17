c63a1de791bbc0b5fade2c7196db23389dd5bdcb
rfdeiaco <ryan.deiaco@gmail.com>
2019-02-17 17:45:07 -0500

    Even vector version uses too much memory.

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <utility>
-#include <unordered_map>
 #include <iostream>
 
 using namespace std;
@@ -9,37 +8,38 @@ using namespace std;
 class Solution {
 public:
   string longestPalindrome(string s) {
-    unordered_map<int, bool> is_palindrome_lut;
-    unordered_map<int, pair<int, int>> max_pair_lut;
-    pair<int, int> max_pair = maxPalindrome(s, 0, s.size(), is_palindrome_lut, max_pair_lut);
+    vector<bool> is_palindrome_lut(s.size() * s.size());
+    vector<pair<int, int>> max_pair_lut(s.size() * s.size());
+    vector<bool> found_lut(s.size() * s.size());
+    pair<int, int> max_pair = maxPalindrome(s, 0, s.size(), is_palindrome_lut, max_pair_lut, found_lut);
 
     return s.substr(max_pair.first, max_pair.second-max_pair.first);
 
   }
 
   pair<int, int> maxPalindrome(string s, int start, int end, 
-    unordered_map<int, bool> &is_palindrome_lut, 
-    unordered_map<int, pair<int, int>> &max_pair_lut) {
+    vector<bool> &is_palindrome_lut, 
+    vector<pair<int, int>> &max_pair_lut,
+    vector<bool> &found_lut) {
 
     if (end - start < 2) {
       return pair<int, int>(start, end);
     }
 
     int search_val = start * s.size() + end;
-    auto it = max_pair_lut.find(search_val);
-    if (it != max_pair_lut.end()) {
-      return it->second;
+    if (found_lut[search_val]) {
+      return max_pair_lut[search_val];
     }
 
-    if (isPalindrome(s, start, end, is_palindrome_lut)) {
+    if (isPalindrome(s, start, end, is_palindrome_lut, found_lut)) {
       pair<int, int> ret_pair(start, end);
       max_pair_lut[search_val] = ret_pair;
       return ret_pair;
     }
 
-    pair<int, int> pair1 = maxPalindrome(s, start + 1, end - 1, is_palindrome_lut, max_pair_lut);
-    pair<int, int> pair2 = maxPalindrome(s, start, end - 1, is_palindrome_lut, max_pair_lut);
-    pair<int, int> pair3 = maxPalindrome(s, start + 1, end, is_palindrome_lut, max_pair_lut);
+    pair<int, int> pair1 = maxPalindrome(s, start + 1, end - 1, is_palindrome_lut, max_pair_lut, found_lut);
+    pair<int, int> pair2 = maxPalindrome(s, start, end - 1, is_palindrome_lut, max_pair_lut, found_lut);
+    pair<int, int> pair3 = maxPalindrome(s, start + 1, end, is_palindrome_lut, max_pair_lut, found_lut);
     int length1 = pair1.second - pair1.first;
     int length2 = pair2.second - pair2.first;
     int length3 = pair3.second - pair3.first;
@@ -59,25 +59,27 @@ public:
 
   }
 
-  bool isPalindrome(string s, int start, int end, unordered_map<int, bool> &is_palindrome_lut) {
+  bool isPalindrome(string s, int start, int end, 
+    vector<bool> &is_palindrome_lut, vector<bool> &found_lut) {
     if (end - start < 2) {
       return true;
     }
 
     int search_val = start * s.size() + end;
-    auto it = is_palindrome_lut.find(search_val);
-    if (it != is_palindrome_lut.end()) {
-      return it->second;
+    if (found_lut[search_val]) {
+      return is_palindrome_lut[search_val];
     }
     
     if (s[start] == s[end-1]) {
-      if (isPalindrome(s, start + 1, end - 1, is_palindrome_lut)) {
+      if (isPalindrome(s, start + 1, end - 1, is_palindrome_lut, found_lut)) {
         is_palindrome_lut[search_val] = true;
+        found_lut[search_val] = true;
         return true;
       }
     }
 
     is_palindrome_lut[search_val] = false;
+    found_lut[search_val] = true;
     return false;
 
   }
